8a5a643dd6f0c8e5940241076f33cb926918a7bb
Felix Gündling <felix.guendling@gmail.com>
2025-06-19 19:46:54 +0200

    wip

--- src/lib/instances.ts
+++ src/lib/instances.ts
@@ -4,12 +4,24 @@ import { spawn } from "child_process";
 export const motisInstances = new Map<number, ReturnType<typeof spawn>>();
 
 export const startInstance = async (port: number) => {
-  const motisProcess = spawn(`${env.MOTIS_FOLDER}/motis/motis`, ['server', '-d', `${env.INSTANCE_FOLDER}/${port}/data`]);
-  motisProcess.stdout.on('data', (data) => {
-    console.log(`stdout[${port}]: ${data}`);
-  });
-  motisProcess.stderr.on('data', (data) => {
-    console.log(`stderr[${port}]: ${data}`);
-  });
-  motisInstances.set(port, motisProcess);
+  const startProcess = () => {
+    const motisProcess = spawn(`${env.MOTIS_FOLDER}/motis/motis`, ['server', '-d', `${env.INSTANCE_FOLDER}/${port}/data`]);
+    motisProcess.stdout.on('data', (data) => {
+      console.log(`stdout[${port}]: ${data}`);
+    });
+    motisProcess.stderr.on('data', (data) => {
+      console.log(`stderr[${port}]: ${data}`);
+    });
+    motisProcess.on('exit', (code, signal) => {
+      // Only restart if the port still exists in the map (was not removed intentionally)
+      if (motisInstances.has(port)) {
+        console.warn(`motis instance on port ${port} exited with code ${code}, signal ${signal}. Restarting...`);
+        setTimeout(startProcess, 1000); // restart after 1s
+      } else {
+        console.log(`motis instance on port ${port} exited and will not be restarted (was removed intentionally).`);
+      }
+    });
+    motisInstances.set(port, motisProcess);
+  };
+  startProcess();
 }
